c65c457bc819725534ea3c745283f301937a0ad3
sawyermade <sawyermade0@gmail.com>
2020-06-26 00:55:49 -0400

    lazy

---
+++ convert_ppd.py
@@ -0,0 +1,17 @@
+import pyautogui, sys
+
+def cvt_png():
+	button = (coord_list[0], coord_list[1])
+	tl, tr = (coord_list[2], coord_list[3]), (coord_list[4], coord_list[5])
+	br, bl = (coord_list[6], coord_list[7]), (coord_list[8], coord_list[9])
+	w, h = tr[0] - tl[0], br[1] - tl[1]
+
+	count = 1
+	pyautogui.screenshot(str(count).zfill(4), region=(tl[0], tl[1], w, h))
+
+def main():
+	coord_list = [int(a) for a in sys.argv[1:]]
+	cvt_png(coord_list)
+
+if __name__ == '__main__':
+	main()
