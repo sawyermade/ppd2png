1c8341b1069604c525bd778b2c11cd6d2b4f31c2
sawyermade <sawyermade0@gmail.com>
2020-06-26 00:59:58 -0400

    lazy

--- convert_ppd.py
+++ convert_ppd.py
@@ -1,4 +1,4 @@
-import pyautogui, sys
+import pyautogui, sys, time
 
 def cvt_png():
 	button = (coord_list[0], coord_list[1])
@@ -6,7 +6,13 @@ def cvt_png():
 	br, bl = (coord_list[6], coord_list[7]), (coord_list[8], coord_list[9])
 	w, h = tr[0] - tl[0], br[1] - tl[1]
 
+	pyautogui.click(x=button[0], y=button[1])
 	count = 1
+	pyautogui.screenshot(str(count).zfill(4), region=(tl[0], tl[1], w, h))
+
+	time.sleep(2)
+	pyautogui.click(x=button[0], y=button[1])
+	count += 1
 	pyautogui.screenshot(str(count).zfill(4), region=(tl[0], tl[1], w, h))
 
 def main():
